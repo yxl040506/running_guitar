b3c6406c5c23bfd82760d8d635c8afc7b9846a76
xinyixyliu <xinyixyliu@tencent.com>
2025-08-15 16:06:44 +0800

    Add Git LFS tracking for large files

--- assets/Scripts/PlayerController.ts
+++ assets/Scripts/PlayerController.ts
@@ -1,14 +1,78 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Vec3, EventMouse, input, Input, Animation } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('PlayerController')
+export const BLOCK_SIZE = 40;
+
+@ccclass("PlayerController")
 export class PlayerController extends Component {
-    start() {
+
+    private _startJump: boolean = false;
+    private _jumpStep: number = 0;
+    private _curJumpTime: number = 0;
+    private _jumpTime: number = 0.1;
+    private _curJumpSpeed: number = 0;
+    private _curPos: Vec3 = new Vec3();
+    private _deltaPos: Vec3 = new Vec3(0, 0, 0);
+    private _targetPos: Vec3 = new Vec3();
+
+    @property(Animation)
+    BodyAnim:Animation = null;
+
+    start () {
+        input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+    }
+
+    reset() {
+    }   
+
+    onMouseUp(event: EventMouse) {
+        if (event.getButton() === 0) {
+            this.jumpByStep(1);
+        } else if (event.getButton() === 2) {
+            this.jumpByStep(2);
+        }
 
     }
 
-    update(deltaTime: number) {
+    jumpByStep(step: number) {
+        if (this._startJump) {
+            return;
+        }
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+
+        const clipName = step == 1 ? 'oneStep' : 'twoStep';
+        const state = this.BodyAnim.getState(clipName);
+        this._jumpTime = state.duration;
+
+        this._curJumpSpeed = this._jumpStep * BLOCK_SIZE/ this._jumpTime;
+        this.node.getPosition(this._curPos);
+        Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep* BLOCK_SIZE, 0, 0));  
         
+        if (this.BodyAnim) {
+            if (step === 1) {
+                this.BodyAnim.play('oneStep');
+            } else if (step === 2) {
+                this.BodyAnim.play('twoStep');
+            }
+        }
+    }
+   
+    update (deltaTime: number) {
+        if (this._startJump) {
+            this._curJumpTime += deltaTime;
+            if (this._curJumpTime > this._jumpTime) {
+                // end
+                this.node.setPosition(this._targetPos);
+                this._startJump = false;              
+            } else {
+                // tween
+                this.node.getPosition(this._curPos);
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                Vec3.add(this._curPos, this._curPos, this._deltaPos);
+                this.node.setPosition(this._curPos);
+            }
+        }
     }
 }
-
